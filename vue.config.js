647573ba515a78aee3bccf73066028afdcda9d97
Collins nyamao <collins.nyamao@strathmore.edu>
2019-10-16 10:57:20 +0300

    feat: selfsigned certs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   devServer: {
     disableHostCheck: true,
-    https: true
+    https: {
+      key: fs.readFileSync(__dirname+'/certs/apache-selfsigned.key'),
+      cert: fs.readFileSync(__dirname+'/certs/apache-selfsigned.crt')
+      /*ca: fs.readFileSync('/path/to/ca.pem'),*/
+    }
   }
 }
